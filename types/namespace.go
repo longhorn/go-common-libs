ac0ac0e1f3a241889b03df258e0e99ef9d13a102
Chin-Ya Huang <chin-ya.huang@suse.com>
2023-07-26 10:46:38 +0800

    feat(namespace): executor
    ref: 3161
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- types/namespace.go
+++ types/namespace.go
@@ -6,3 +6,17 @@ const (
 	HostProcDirectory = "/host/proc"
 	ProcDirectory     = "/proc"
 )
+
+const NsBinary = "nsenter"
+
+type Namespace string
+
+const (
+	NamespaceIpc = Namespace("ipc")
+	NamespaceMnt = Namespace("mnt")
+	NamespaceNet = Namespace("net")
+)
+
+func (ns Namespace) String() string {
+	return string(ns)
+}
