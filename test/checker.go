0af06c8124cf85df7f5bb3dd23ed15cfcdd11dc7
Vicente Cheng <vicente.cheng@suse.com>
2023-12-28 10:48:23 +0800

    utils: add some utils for grpc component
        - add `GetGRPCAddress`
        - add `GenerateRandomNumber`
        - add `IsInList` checker for test
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>
    Co-authored-by: c3y1huang <chin-ya.huang@suse.com>

---
+++ test/checker.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+	"fmt"
+	"reflect"
+
+	check "gopkg.in/check.v1"
+)
+
+// isInListChecker verifies if an element is present in the provided list.
+// The list must be a slice or array, and the element must be the same type as the list.
+//
+// For example:
+//
+//     c.Assert("b", IsInList, []string{"a", "b", "c"})
+//     c.Assert(2, IsInList, []int{1, 2, 3})
+//
+
+type isInListChecker struct {
+	*check.CheckerInfo
+}
+
+var IsInList check.Checker = &isInListChecker{
+	&check.CheckerInfo{
+		Name:   "IsInList",
+		Params: []string{"element", "list"},
+	},
+}
+
+func (checker *isInListChecker) Check(params []interface{}, names []string) (result bool, error string) {
+	defer func() {
+		if r := recover(); r != nil {
+			result = false
+			error = fmt.Sprint(r)
+		}
+	}()
+
+	element := params[0]
+	list := params[1]
+
+	valueOfList := reflect.ValueOf(list)
+	if valueOfList.Kind() != reflect.Slice && valueOfList.Kind() != reflect.Array {
+		return false, fmt.Sprintf("container must be a slice or array, got %T", valueOfList.Type())
+	}
+
+	if valueOfList.Len() == 0 {
+		return false, "container should not be empty"
+	}
+
+	for i := 0; i < valueOfList.Len(); i++ {
+		if valueOfList.Index(i).Interface() == element {
+			return true, ""
+		}
+	}
+	return false, ""
+}
