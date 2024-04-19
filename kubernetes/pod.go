c7925c68fc79d37c0bbfcf32829d8f0f0abf6c0c
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-04-19 16:55:50 +0800

    test(kubernetes): IsPodContainerInState
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- kubernetes/pod.go
+++ kubernetes/pod.go
@@ -4,26 +4,34 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// IsContainerCompleted returns true if the container is terminated with exit code 0.
 func IsContainerCompleted(status *corev1.ContainerStatus) bool {
 	return status.State.Terminated != nil && status.State.Terminated.ExitCode == 0
 }
 
+// IsContainerInitializing returns true if the container is waiting for initialization.
 func IsContainerInitializing(status *corev1.ContainerStatus) bool {
 	return status.State.Waiting != nil && status.State.Waiting.Reason == "PodInitializing"
 }
 
+// IsContainerReady returns true if the container is ready.
 func IsContainerReady(status *corev1.ContainerStatus) bool {
 	return status.Ready
 }
 
+// IsContainerRestarted returns true if the container is terminated and restarted at least once.
 func IsContainerRestarted(status *corev1.ContainerStatus) bool {
 	return status.State.Terminated != nil && status.RestartCount > 0
 }
 
+// IsContainerWaitingCrashLoopBackOff returns true if the container is waiting for crash loop back off.
 func IsContainerWaitingCrashLoopBackOff(status *corev1.ContainerStatus) bool {
 	return status.State.Waiting != nil && status.State.Waiting.Reason == "CrashLoopBackOff"
 }
 
+// IsPodContainerInState checks if a container is in a desired state.
+// The function searches through the container statuses of the pod of a pod (including init containers)
+// to determine if a specific container is in the desired state of the conditionFunc.
 func IsPodContainerInState(pod *corev1.Pod, containerName string, conditionFunc func(*corev1.ContainerStatus) bool) bool {
 	containerStatuses := pod.DeepCopy().Status.InitContainerStatuses
 	containerStatuses = append(containerStatuses, pod.DeepCopy().Status.ContainerStatuses...)
