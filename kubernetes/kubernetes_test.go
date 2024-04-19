a2c2e91f7ebe7907912d1588ed03cafcfed098a0
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-04-19 11:49:23 +0800

    test(kubernetes): GetInClusterConfig
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- kubernetes/kubernetes_test.go
+++ kubernetes/kubernetes_test.go
@@ -5,7 +5,7 @@ import (
 
 	. "gopkg.in/check.v1"
 
-	_ "github.com/longhorn/go-common-libs/test"
+	"github.com/longhorn/go-common-libs/test"
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -13,3 +13,25 @@ func Test(t *testing.T) { TestingT(t) }
 type TestSuite struct{}
 
 var _ = Suite(&TestSuite{})
+
+func (s *TestSuite) TestGetInClusterConfig(c *C) {
+	type testCase struct {
+		expectError bool
+	}
+	testCases := map[string]testCase{
+		"GetInClusterConfig(...): not in cluster": {
+			expectError: true,
+		},
+	}
+	for testName, testCase := range testCases {
+		c.Logf("testing kubernetes.%v", testName)
+
+		_, err := GetInClusterConfig()
+
+		if testCase.expectError {
+			c.Assert(err, NotNil, Commentf(test.ErrErrorFmt, testName, err))
+			continue
+		}
+		c.Assert(err, IsNil, Commentf(test.ErrErrorFmt, testName, err))
+	}
+}
