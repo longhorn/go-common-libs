31048e30743c3bf9dc26717218564551d2804053
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-07-24 16:44:57 +0800

    chore(test/net): replace gocheck suite with Go's t.Run subtests
    longhorn/longhorn-11372
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- net/ip_test.go
+++ net/ip_test.go
@@ -3,105 +3,105 @@ package net
 import (
 	"net"
 	"strings"
+	"testing"
 
 	. "gopkg.in/check.v1"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/longhorn/go-common-libs/test"
 )
 
-func (s *TestSuite) TestIsLoopbackHost(c *C) {
+func TestIsLoopbackHost(t *testing.T) {
 	type testCase struct {
 		host     string
 		expected bool
 	}
 
 	testCases := map[string]testCase{
-		"IsLoopbackHost(...): localhost": {
+		"Localhost": {
 			host:     "localhost",
 			expected: true,
 		},
-		"IsLoopbackHost(...): 127.0.0.1": {
+		"127.0.0.1": {
 			host:     "127.0.0.1",
 			expected: true,
 		},
-		"IsLoopbackHost(...): 0.0.0.0": {
+		"0.0.0.0": {
 			host:     "0.0.0.0",
 			expected: true,
 		},
-		"IsLoopbackHost(...): ::1": {
+		"::1": {
 			host:     "::1",
 			expected: true,
 		},
-		"IsLoopbackHost(...): ": {
+		"Empty": {
 			host:     "",
 			expected: true,
 		},
-		"IsLoopbackHost(...): 8.8.8.8": {
+		"8.8.8.8": {
 			host:     "8.8.8.8",
 			expected: false,
 		},
 	}
 
 	for testName, testCase := range testCases {
-		c.Logf("testing net.%v", testName)
-
-		result := IsLoopbackHost(testCase.host)
-		c.Assert(result, Equals, testCase.expected, Commentf(test.ErrResultFmt, testName))
+		t.Run(testName, func(t *testing.T) {
+			result := IsLoopbackHost(testCase.host)
+			assert.Equal(t, testCase.expected, result, Commentf(test.ErrResultFmt, testName))
+		})
 	}
 }
 
-func (s *TestSuite) TestGetLocalIPv4fromInterface(c *C) {
+func TestGetLocalIPv4fromInterface(t *testing.T) {
 	type testCase struct {
 		host     string
 		expected bool
 	}
 
 	testCases := map[string]testCase{
-		"GetLocalIPv4fromInterface(...):": {
+		"Local": {
 			host:     "",
 			expected: true,
 		},
 	}
 
 	for testName := range testCases {
-		c.Logf("testing net.%v", testName)
+		t.Run(testName, func(t *testing.T) {
+			interfaces, err := net.Interfaces()
+			assert.Nil(t, err, Commentf(test.ErrErrorFmt, testName, err))
 
-		interfaces, err := net.Interfaces()
-		c.Assert(err, IsNil)
+			for _, iface := range interfaces {
+				ip, err := GetLocalIPv4fromInterface(iface.Name)
+				if err != nil {
+					assert.True(t, strings.Contains(err.Error(), "don't have an IPv4 address"), Commentf(test.ErrResultFmt, testName))
+					continue
+				}
 
-		for _, iface := range interfaces {
-			ip, err := GetLocalIPv4fromInterface(iface.Name)
-			if err != nil {
-				c.Assert(strings.Contains(err.Error(), "don't have an IPv4 address"),
-					Equals, true, Commentf(test.ErrResultFmt, testName))
-				continue
+				assert.True(t, isIPv4(ip), Commentf(test.ErrResultFmt, testName))
 			}
-
-			c.Assert(isIPv4(ip), Equals, true, Commentf(test.ErrResultFmt, testName))
-		}
+		})
 	}
 }
 
-func (s *TestSuite) TestGetAnyExternalIP(c *C) {
+func TestGetAnyExternalIP(t *testing.T) {
 	type testCase struct {
 		host     string
 		expected bool
 	}
 
 	testCases := map[string]testCase{
-		"GetLocalIPv4fromInterface(...):": {
+		"Local": {
 			host:     "",
 			expected: true,
 		},
 	}
 
 	for testName := range testCases {
-		c.Logf("testing net.%v", testName)
-
-		ip, err := GetAnyExternalIP()
-
-		c.Assert(err, IsNil)
-		c.Assert(isIPv4(ip), Equals, true, Commentf(test.ErrResultFmt, testName))
+		t.Run(testName, func(t *testing.T) {
+			ip, err := GetAnyExternalIP()
+			assert.Nil(t, err, Commentf(test.ErrErrorFmt, testName, err))
+			assert.True(t, isIPv4(ip), Commentf(test.ErrResultFmt, testName))
+		})
 	}
 }
 
