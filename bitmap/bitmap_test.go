d896331cff111e64c4284bbd4b0dc108fa988971
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-07-25 08:00:52 +0800

    chore(test/bitmap): replace gocheck suite with Go's t.Run subtests
    longhorn/longhorn-11372
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- bitmap/bitmap_test.go
+++ bitmap/bitmap_test.go
@@ -3,127 +3,135 @@ package bitmap
 import (
 	"testing"
 
-	. "gopkg.in/check.v1"
+	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func TestBitmap(t *testing.T) {
+	t.Run("NewBitmap with invalid range", func(t *testing.T) {
+		bm, err := NewBitmap(200, 100)
+		assert.Nil(t, bm)
+		assert.NotNil(t, err)
+	})
 
-type TestSuite struct{}
+	t.Run("Basic allocation and release", func(t *testing.T) {
+		bm, err := NewBitmap(100, 200)
+		assert.NoError(t, err)
 
-var _ = Suite(&TestSuite{})
+		_, _, err = bm.AllocateRange(0)
+		assert.Error(t, err)
 
-func (s *TestSuite) TestBitmap(c *C) {
-	bm, err := NewBitmap(200, 100)
-	c.Assert(bm, IsNil)
-	c.Assert(err, NotNil)
+		start, end, err := bm.AllocateRange(100)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(100), start)
+		assert.Equal(t, int32(199), end)
 
-	bm, err = NewBitmap(100, 200)
-	c.Assert(err, IsNil)
+		start, end, err = bm.AllocateRange(1)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(200), start)
+		assert.Equal(t, int32(200), end)
 
-	_, _, err = bm.AllocateRange(0)
-	c.Assert(err, NotNil)
+		_, _, err = bm.AllocateRange(1)
+		assert.Error(t, err)
 
-	start, end, err := bm.AllocateRange(100)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(100))
-	c.Assert(end, Equals, int32(199))
+		err = bm.ReleaseRange(100, 100)
+		assert.NoError(t, err)
 
-	start, end, err = bm.AllocateRange(1)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(200))
-	c.Assert(end, Equals, int32(200))
+		start, end, err = bm.AllocateRange(1)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(100), start)
+		assert.Equal(t, int32(100), end)
 
-	_, _, err = bm.AllocateRange(1)
-	c.Assert(err, NotNil)
+		err = bm.ReleaseRange(105, 120)
+		assert.NoError(t, err)
 
-	err = bm.ReleaseRange(100, 100)
-	c.Assert(err, IsNil)
+		start, end, err = bm.AllocateRange(15)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(105), start)
+		assert.Equal(t, int32(119), end)
 
-	start, end, err = bm.AllocateRange(1)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(100))
-	c.Assert(end, Equals, int32(100))
+		_, _, err = bm.AllocateRange(2)
+		assert.Error(t, err)
 
-	err = bm.ReleaseRange(105, 120)
-	c.Assert(err, IsNil)
+		start, end, err = bm.AllocateRange(1)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(120), start)
+		assert.Equal(t, int32(120), end)
 
-	start, end, err = bm.AllocateRange(15)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(105))
-	c.Assert(end, Equals, int32(119))
+		// No-op
+		err = bm.ReleaseRange(0, 0)
+		assert.NoError(t, err)
 
-	_, _, err = bm.AllocateRange(2)
-	c.Assert(err, NotNil)
-
-	start, end, err = bm.AllocateRange(1)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(120))
-	c.Assert(end, Equals, int32(120))
-
-	// No-op
-	err = bm.ReleaseRange(0, 0)
-	c.Assert(err, IsNil)
-
-	err = bm.ReleaseRange(0, 200)
-	c.Assert(err, NotNil)
+		err = bm.ReleaseRange(0, 200)
+		assert.Error(t, err)
+	})
 }
 
-func (s *TestSuite) TestBitmap2(c *C) {
+func TestBitmap2(t *testing.T) {
 	bm, _ := NewBitmap(100, 200)
 
-	start, end, err := bm.AllocateRange(50)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(100))
-	c.Assert(end, Equals, int32(149))
+	t.Run("Initial allocation", func(t *testing.T) {
+		start, end, err := bm.AllocateRange(50)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(100), start)
+		assert.Equal(t, int32(149), end)
+	})
 
-	err = bm.ReleaseRange(149, 100)
-	c.Assert(err, NotNil)
+	t.Run("Invalid release", func(t *testing.T) {
+		err := bm.ReleaseRange(149, 100)
+		assert.NotNil(t, err)
+	})
 
-	err = bm.ReleaseRange(100, 149)
-	c.Assert(err, IsNil)
+	t.Run("Valid release and reuse", func(t *testing.T) {
+		err := bm.ReleaseRange(100, 149)
+		assert.NoError(t, err)
 
-	// Even though freed, it will start looking after last successful.
-	start, end, err = bm.AllocateRange(1)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(150))
-	c.Assert(end, Equals, int32(150))
+		// Even though freed, it will start looking after last successful.
+		start, end, err := bm.AllocateRange(1)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(150), start)
+		assert.Equal(t, int32(150), end)
+	})
 
-	// There are 100 free, but not contiguous
-	_, _, err = bm.AllocateRange(100)
-	c.Assert(err, NotNil)
+	t.Run("Allocate fragmented", func(t *testing.T) {
+		// There are 100 free, but not contiguous
+		_, _, err := bm.AllocateRange(100)
+		assert.NotNil(t, err)
 
-	start, end, err = bm.AllocateRange(40)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(151))
-	c.Assert(end, Equals, int32(190))
+		start, end, err := bm.AllocateRange(40)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(151), start)
+		assert.Equal(t, int32(190), end)
 
-	// This will work, but only on the fallback search.
-	start, end, err = bm.AllocateRange(50)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(100))
-	c.Assert(end, Equals, int32(149))
+		// This will work, but only on the fallback search.
+		start, end, err = bm.AllocateRange(50)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(100), start)
+		assert.Equal(t, int32(149), end)
 
-	_, _, err = bm.AllocateRange(20)
-	c.Assert(err, NotNil)
+		_, _, err = bm.AllocateRange(20)
+		assert.NotNil(t, err)
 
-	start, end, err = bm.AllocateRange(10)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(191))
-	c.Assert(end, Equals, int32(200))
+		start, end, err = bm.AllocateRange(10)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(191), start)
+		assert.Equal(t, int32(200), end)
 
-	_, _, err = bm.AllocateRange(1)
-	c.Assert(err, NotNil)
+		_, _, err = bm.AllocateRange(1)
+		assert.NotNil(t, err)
+	})
 
-	err = bm.ReleaseRange(120, 149)
-	c.Assert(err, IsNil)
+	t.Run("Partial release and reuse", func(t *testing.T) {
+		err := bm.ReleaseRange(120, 149)
+		assert.NoError(t, err)
 
-	start, end, err = bm.AllocateRange(10)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(120))
-	c.Assert(end, Equals, int32(129))
+		start, end, err := bm.AllocateRange(10)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(120), start)
+		assert.Equal(t, int32(129), end)
 
-	start, end, err = bm.AllocateRange(1)
-	c.Assert(err, IsNil)
-	c.Assert(start, Equals, int32(130))
-	c.Assert(end, Equals, int32(130))
+		start, end, err = bm.AllocateRange(1)
+		assert.NoError(t, err)
+		assert.Equal(t, int32(130), start)
+		assert.Equal(t, int32(130), end)
+	})
 }
